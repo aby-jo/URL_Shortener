2342a6bc0cd8249cd6a60d0a2781340be8606f5d
aby-jo <173944472+aby-jo@users.noreply.github.com>
2025-05-31 04:36:51 +0530

    Initialize AccessLog model for tracking URL clicks

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 from database import Base 
-from sqlalchemy import Column,Integer,String,DateTime,func
+from sqlalchemy import Column,Integer,String,DateTime,func,ForeignKey
+from sqlalchemy.orm import relationship
 
 class ShortURL(Base):
     __tablename__='short_url'
@@ -8,3 +9,10 @@ class ShortURL(Base):
     short_code=Column(String,unique=True,nullable=False,index=True)
     created_at=Column(DateTime(timezone=True),server_default=func.now())
     visit_count=Column(Integer,default=0)
+    access_log=relationship("AccessLog",back_populates="short_url")
+class AccessLog(Base):
+    __tablename__="access_log"
+    uuid=Column(Integer,primary_key=True,index=True,autoincrement=True)
+    short_code=Column(String,ForeignKey('short_url.short_code'),nullable=False,index=True)
+    time_stamp=Column(DateTime(timezone=True),server_default=func.now())
+    short_url=relationship("ShortURL",back_populates="access_log")
