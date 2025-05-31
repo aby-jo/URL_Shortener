689f6bb58e13e58125c35c00b6bba68c968cae2b
aby-jo <173944472+aby-jo@users.noreply.github.com>
2025-05-31 18:09:03 +0530

    Added metadata for docs

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
 from fastapi import FastAPI,HTTPException,Depends,Query
 from fastapi.responses import RedirectResponse
 from fastapi.middleware.cors import CORSMiddleware
+
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import IntegrityError
-from pydantic import BaseModel,HttpUrl
+
+from pydantic import BaseModel,HttpUrl,Field
+
 from hashlib import sha256
 import base64
+
+from datetime import datetime
+
 from database import engine,SessionLocal
 import model
 
-app=FastAPI()
+app=FastAPI(title="URL Shortener API",description="A simple API to shorten and resolve URLs.")
 origins=['http://localhost:8000']
 app.add_middleware(CORSMiddleware,allow_origins=origins,allow_headers=["*"],allow_methods=["*"],allow_credentials=True)
        
 model.Base.metadata.create_all(bind=engine)
-@app.get('/hello')
+@app.get('/hello',description="hello")
 def hello():
     return "Hi, How you doing ?"
-@app.get("/")
+@app.get("/",summary="Github link",description="Returns github repository link")
 def about():
-    return {"about":"About Page"}
+    return {"About Page":"https://github.com/aby-jo/URL_Shortener"}
 
 def get_db():
     db=SessionLocal()
@@ -29,8 +35,12 @@ def get_db():
         db.close()
 
 class URLRequest(BaseModel):
-    long_url:HttpUrl
-@app.post('/shorten')
+    long_url:HttpUrl= Field(..., description="The original long URL to shorten")
+class URLResponse(BaseModel):
+    id: str = Field(..., description="Unique ID of the shortened URL record")
+    short_code: str = Field(..., description="Generated short code")
+    created_at: datetime = Field(..., description="Timestamp when the short URL was created")
+@app.post('/shorten',response_model=URLResponse,summary="Create short URL", description="Generate a short URL code from a long URL.")
 def shorten(request: URLRequest,db: Session=Depends(get_db)):
     url=str(request.long_url)
     exisiting_url=db.query(model.ShortURL).filter(model.ShortURL.original_url==url).first()
@@ -71,7 +81,7 @@ def short_code_generator(url):
     return output
 
 
-@app.get('/show')
+@app.get('/show',summary="Show all short codes",description="Returns all short codes and their original URLs if query parameter `show=all` is provided.")
 def get_all_codes(show:str=Query(None),db:Session=Depends(get_db)):
     if show=="all":
         results=db.query(model.ShortURL.short_code,model.ShortURL.original_url).all()
@@ -80,7 +90,7 @@ def get_all_codes(show:str=Query(None),db:Session=Depends(get_db)):
         return short_urls
     return {"error": "Invalid query parameter"}
 
-@app.get('/admin')
+@app.get('/admin',summary="Get access logs of a shortcode",description="Returns the 10 most recent timestamps for a given shortcode, along with its total visit count.")
 def get_access_logs(passwrd:str=Query(None),code:str=Query(None),db:Session=Depends(get_db)):
     if passwrd!="all":
         return {"error": "Unauthorized Access"}
@@ -94,7 +104,7 @@ def get_access_logs(passwrd:str=Query(None),code:str=Query(None),db:Session=Depe
             "access_logs":access_logs}
     
 
-@app.get('/{code}')
+@app.get('/{code}',summary="Get the original url of the short code",description="Returns a redirect to the original url")
 def resolve(code:str, db: Session=Depends(get_db)):
     org_link=db.query(model.ShortURL).filter(model.ShortURL.short_code==code).first()
     if org_link:
